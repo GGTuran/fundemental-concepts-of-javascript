f41541ebb4f9f5ec9357836acfdecacb54706c7f
GGTuran <tayyabturan67@gmail.com>
2024-03-31 20:44:49 +0600

    commiting it for further use

---
+++ comparison.js
@@ -0,0 +1,7 @@
+// console.log(5 > 10);
+// console.log(15 >12);
+// ternary operator
+// condition ? true action : false action
+
+// const age = 20;
+// age>= 18 ? console.log('Adult') : console.log('Teen')
