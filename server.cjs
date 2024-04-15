b3811ef74619ae324f3d8730f1b8348dff3ff3c5
GGTuran <tayyabturan67@gmail.com>
2024-04-15 15:49:58 +0600

    query

--- server.cjs
+++ server.cjs
@@ -1,9 +1,51 @@
 const http = require("node:http");
+const url = require("url");
+
+const posts = [
+    {
+      "id" : 1,
+      "name": "Alice",
+      "age": 25,
+      "email": "alice@example.com"
+    },
+    {
+        "id" : 2, 
+      "name": "Bob",
+      "age": 30,
+      "email": "bob@example.com"
+    },
+    {
+        "id" : 3,
+      "name": "Charlie",
+      "age": 35,
+      "email": "charlie@example.com"
+    }
+  ]
 
 const server = http.createServer( (req, res) =>{
-    // console.log(req)
+    console.log(req.url)
     // console.log(res)
-    res.end("Hello from backend");
+    const parsedURL =  new URL(req.url , `http://${req.headers.host}`)
+
+    const pathName = parsedURL.pathname
+
+    if(pathName === "/home" && req.method === "GET"){
+        res.end("Home sweet home")
+    } else if (pathName === "/posts" && req.method === "GET"){
+
+        const query = parsedURL.searchParams;
+        const postId = query.get("id");
+        const expectedPost = posts.find((post) => post.id == postId);
+
+        res.writeHead(200 , {
+            "Content-type" : "application/json",
+        });
+        res.end(JSON.stringify(expectedPost))
+    }
+
+
+
+    // res.end("Hello from backend");
 }) ;
 
 server.listen(5000, "127.0.0.1", () =>{
