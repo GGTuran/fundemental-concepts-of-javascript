59ec01f35b89d83b1e82e5adc7f01ce393da495d
GGTuran <tayyabturan67@gmail.com>
2024-04-15 16:06:56 +0600

    used another method

--- server.cjs
+++ server.cjs
@@ -1,26 +1,27 @@
 const http = require("node:http");
 const url = require("url");
-
-const posts = [
-    {
-      "id" : 1,
-      "name": "Alice",
-      "age": 25,
-      "email": "alice@example.com"
-    },
-    {
-        "id" : 2, 
-      "name": "Bob",
-      "age": 30,
-      "email": "bob@example.com"
-    },
-    {
-        "id" : 3,
-      "name": "Charlie",
-      "age": 35,
-      "email": "charlie@example.com"
-    }
-  ]
+const path = require("node:path");
+const fs = require('fs');
+// const posts = [
+//     {
+//       "id" : 1,
+//       "name": "Alice",
+//       "age": 25,
+//       "email": "alice@example.com"
+//     },
+//     {
+//         "id" : 2, 
+//       "name": "Bob",
+//       "age": 30,
+//       "email": "bob@example.com"
+//     },
+//     {
+//         "id" : 3,
+//       "name": "Charlie",
+//       "age": 35,
+//       "email": "charlie@example.com"
+//     }
+//   ]
 
 const server = http.createServer( (req, res) =>{
     console.log(req.url)
@@ -33,14 +34,22 @@ const server = http.createServer( (req, res) =>{
         res.end("Home sweet home")
     } else if (pathName === "/posts" && req.method === "GET"){
 
-        const query = parsedURL.searchParams;
-        const postId = query.get("id");
-        const expectedPost = posts.find((post) => post.id == postId);
+      
 
-        res.writeHead(200 , {
-            "Content-type" : "application/json",
-        });
-        res.end(JSON.stringify(expectedPost))
+        fs.readFile(__dirname + "/posts.json", "utf-8", (err , data) =>{
+            if(err){
+                throw new Error("Error");
+            }
+              const query = parsedURL.searchParams;
+        const postId = query.get("id");
+        const expectedPost = JSON.parse(data).find((post) => post.id == postId);
+            res.writeHead(200 , {
+                "Content-type" : "application/json",
+            });
+            res.end(JSON.stringify(expectedPost))
+        } )
+
+   
     }
 
 
